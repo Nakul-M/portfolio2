19ae60a4bdb9152a6c27643c8230b7c8d5398a33
nakulmahajan.ac.in@gmail.com <nakulmahajan.ac.in@gmail.com>
2025-12-26 20:42:55 +0530

    4

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,16 @@ import SocialLinks from "@/components/SocialLinks";
 import HeroSection from "@/sections/HeroSection";
 import AboutSection from "@/sections/AboutSection";
 import ProjectSection from "@/sections/ProjectSection";
-
+import BlogSection from "@/sections/BlogSection";
 import ContactSection from "@/sections/ContactSection";
 import Footer from "@/components/Footer";
 
+import { getAllPosts } from "utils/api";
+import { MdxMeta } from "../pages/blog/posts/[slug]";
 
-
+type Props = {
+  blogPosts: MdxMeta[];
+};
 
 export const meta = {
   description:
@@ -25,8 +29,50 @@ export const meta = {
   imageAlt: "Sat Naing portfolio website",
 };
 
+const Home: NextPage<Props> = ({ blogPosts }) => {
+  return (
+    <>
+      <AppHead
+        title="Sat Naing - A Full-stack Developer"
+        url={`${process.env.NEXT_PUBLIC_URL}`}
+        meta={meta}
+      />
+      <Loader>Nakul Mahajan</Loader>
+      <div className="bg-bglight dark:bg-bgdark overflow-hidden">
+        <div className="selection:bg-marrsgreen selection:text-bglight dark:selection:bg-carrigreen dark:selection:text-bgdark">
+          <SkipToMain />
+          <Header />
+          <main id="main">
+            <HeroSection />
+            <AboutSection />
+            <ProjectSection />
+      
+            <ContactSection />
+          </main>
+          <SocialLinks page="index" />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
 
+export const getStaticProps: GetStaticProps = async () => {
+  const blogPosts = getAllPosts([
+    "coverImage",
+    "coverImageAlt",
+    "slug",
+    "title",
+    "excerpt",
+    "datetime",
+    "featured",
+  ]);
 
+  return {
+    props: {
+      blogPosts,
+    },
+  };
+};
 
-
-
+export default Home;
